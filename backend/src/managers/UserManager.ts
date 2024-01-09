86e82313da34d49bf100ca783521759c0f3dd492
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-01-09 21:49:12 +0530

    fix : minor fix

--- backend/src/managers/UserManager.ts
+++ backend/src/managers/UserManager.ts
@@ -44,7 +44,7 @@ export class UserManager {
       return;
     }
 
-    const room = this.roomManager.createRoom(user1, user1);
+    const room = this.roomManager.createRoom(user1, user2);
     this.clearQueue();
   }
 
