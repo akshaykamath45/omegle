62051dbaa21cb124acbfe424ce91fd777ac6f7ec
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-01-09 22:42:39 +0530

    feat : establishing connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,28 @@
 import { Socket } from "socket.io";
 import http from "http";
-
-const express = require('express');
-const { Server } = require('socket.io');
+const express = require("express");
+import { Server } from "socket.io";
+import { UserManager } from "./managers/UserManager";
 
 const app = express();
 const server = http.createServer(http);
 
-const io = new Server(server);
-
-io.on('connection', (socket: Socket) => {
-  console.log('a user connected');
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
+const userManager = new UserManager();
+
+io.on("connection", (socket: Socket) => {
+  console.log("a user connected");
+  userManager.addUser("randomName", socket);
+  socket.on("disconnect", () => {
+    userManager.removeUser(socket.id);
+  });
+});
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
-  });
+  console.log("listening on *:3000");
+});
