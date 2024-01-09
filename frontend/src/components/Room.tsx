d0b8d424d595d4fab27bbe6091dadbf2f16d8819
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-01-09 21:54:34 +0530

    chore : imported socket.io-client modules

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-import { Socket, io } from "socket.io";
+import { Socket, io } from "socket.io-client";
 
 const Room = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const name = searchParams.get("name");
 
   useEffect(() => {
-    // logic to init user to the room
+    
   }, [name]);
 
   return <div>Hi {name}</div>;
